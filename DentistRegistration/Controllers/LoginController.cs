9d6316e001f80c5e50eeb5e315f5fb81ff80a728
nickolasmalyk <nickolas.malyk@gmail.com>
2018-04-26 21:13:06 +0300

    add user Controller and add Swagger in project

---
+++ DentistRegistration/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using DentistRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TestDemo.Controllers
+{
+    [EnableCors(origins: "http://localhost:9090", headers: "*", methods: "*")]
+    public class LoginController : ApiController
+    {
+        LoginUserDataAccessLayer loginer = new LoginUserDataAccessLayer();
+
+        [HttpPost]
+        public IHttpActionResult Login([FromBody]LoginViewModel user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                if (loginer.Login(user))
+                {
+                    return Ok("Logined.");
+                }
+                return BadRequest("Invalid login or password");
+
+            }
+            catch (Exception)
+            {
+                return Ok("Something went wrong");
+            }
+        }
+
+    }
+}
---
+++ DentistRegistration/Models/LoginUserDataAccessLayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DentistRegistration.Models
+{
+    public class LoginUserDataAccessLayer
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        int count;
+
+        // check whether there is a user with such a login and password
+        public bool Login(LoginViewModel user)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand($"select count(id_User) from users where PHONENUM = {user.Phonenum} and USER_PASSWORD = '{user.Password}'", con);
+
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                count = Convert.ToInt32(reader.GetValue(0));
+                }
+                 con.Close();
+
+                if (count > 0)
+                    return true;
+                return false;
+            }
+        }
+    
+}
+}
