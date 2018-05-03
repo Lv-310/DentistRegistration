6df7be6259eb2e8ac7aa1f9309d96ec59e5ed49c
Andriy Lavrushko <Ondzeylav@gmail.com>
2018-05-04 01:55:17 +0300

    Add class EncryptingConfiguration

---
+++ DentistRegistration/Servises/EncryptingConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.Web.Configuration;
+
+namespace DentistRegistration.Servises
+{
+    public sealed class EncryptingConfiguration
+    {
+        public static void ProtectSection(string sectionName,
+                                   string provider)
+        {
+            Configuration config =
+                WebConfigurationManager.
+                    OpenWebConfiguration("/");
+
+            ConfigurationSection section =
+                         config.GetSection(sectionName);
+
+            if (section != null &&
+                      !section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.ProtectSection(provider);
+                config.Save();
+            }
+        }
+
+        public static void UnProtectSection(string sectionName)
+        {
+            Configuration config =
+                WebConfigurationManager.
+                    OpenWebConfiguration("/");
+
+            ConfigurationSection section =
+                      config.GetSection(sectionName);
+
+            if (section != null &&
+                  section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.UnprotectSection();
+                config.Save();
+            }
+        }
+    }
+}
