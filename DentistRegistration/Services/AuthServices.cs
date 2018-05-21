81eae5293d0c97482385df6a5a2c41c2b9b8f0b6
Unnamed999999999 <Unnamed999999999@Gmail.com>
2018-05-21 20:09:18 +0300

    Access token refactoring

--- DentistRegistration/Services/AuthServices.cs
+++ DentistRegistration/Services/AuthServices.cs
@@ -10,15 +10,8 @@ namespace DentistRegistration.Servises
 {
     public class AuthServices
     {
-        public string GetAccessToken(string phoneNumber)
+        public string GetAccessToken(IEnumerable<Claim> claims)
         {
-            // new Guid("37EEC6AC-CFEF-44B7-8E3B-4EA10A7A7203")
-            var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.MobilePhone, phoneNumber),
-                    new Claim(ClaimTypes.Name, Guid.NewGuid().ToString())
-                };
-
             var securityKey = new SymmetricSecurityKey(AllConstants.JwtTokenConstants.Secret);
             var cred = new SigningCredentials(
                 securityKey,
@@ -45,20 +38,7 @@ namespace DentistRegistration.Servises
                     new Claim("Id",user.Id.ToString())
                 };
 
-            var securityKey = new SymmetricSecurityKey(AllConstants.JwtTokenConstants.Secret);
-            var cred = new SigningCredentials(
-                securityKey,
-                SecurityAlgorithms.HmacSha256Signature,
-                SecurityAlgorithms.HmacSha256Signature);
-
-            var token = new JwtSecurityToken(
-                issuer: AllConstants.JwtTokenConstants.Issuer,
-                audience: AllConstants.JwtTokenConstants.Audience,
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(10),
-                signingCredentials: cred);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
+            return GetAccessToken(claims);
         }
     }
 }
