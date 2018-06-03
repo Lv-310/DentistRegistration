4a9e6142557fee810a2ad4ed8740425b694caf08
taras21777 <piatak.taras@gmail.com>
2018-06-03 20:08:33 +0300

    mb fix

--- src/components/visitpage/Visitpage.js
+++ src/components/visitpage/Visitpage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import VisitProfileCard from './VisitProfileCard'
-import Jaw from '../userhomepage/Jaw'
 
 class Visitpage extends React.Component{ 
 
