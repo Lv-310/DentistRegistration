dceba8283ccfac28d0254625bdaf8ba6031351d7
taras21777 <piatak.taras@gmail.com>
2018-06-04 13:27:10 +0300

    debug

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -296,12 +296,14 @@ validateForm() {
         </div>
         <BigCalendar
           events={this.state.allevents}
-          defaultView={currentView}
+          defaultView={this.changeDefaultView()}
           scrollToTime={new Date(1970, 1, 1, 6)}
           defaultDate={currentDate}
           views={this.checkIfMobile()}
           min={new Date(2017, 10, 0, 8, 0, 0)}
           max={new Date(2017, 10, 0, 20, 0, 0)}
+          onNavigate={date => this.props.history.push(`/Home/doctor/${this.props.match.params.doctorId}/${this.formatURLDate(date)}/${this.props.match.params.view}`)}
+          onView={view => this.props.history.push(`/Home/doctor/${this.props.match.params.doctorId}/${this.props.match.params.date}/${view}`)}
           onSelectEvent={event => this.onEventClick(event)}
           components={{
             toolbar: Toolbar
