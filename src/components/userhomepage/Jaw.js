646c8464efec3f4612765acd53cf2a2ad914de53
Andriy Lavrushko <Ondzeylav@gmail.com>
2018-06-06 13:47:33 +0300

    simplify imagination of teeth in Jaw component

--- src/components/userhomepage/Jaw.js
+++ src/components/userhomepage/Jaw.js
@@ -32,50 +32,29 @@ class Jaw extends React.Component {
 
     render() { 
         
-        let arrayUpLeft = [18,17,16,15,14,13,12,11]
-        let arrayUpRight = [21,22,23,24,25,26,27,28]
-        let arrayDownRight = [31,32,33,34,35,36,37,38]
-        let arrayDownLeft = [48,47,46,45,44,43,42,41]
+        let arrayUp = [18,17,16,15,14,13,12,11,21,22,23,24,25,26,27,28]
+        let arrayDown = [48,47,46,45,44,43,42,41,31,32,33,34,35,36,37,38]
         
-        let imagesUpLeft = arrayUpLeft.map(image => {
+        let imagesUp = arrayUp.map(image => {
             return <div data-toggle="collapse" data-target="#collapseExample" key={image} className={(`tooth-${image}`)} onClick={() =>this.info(image)}>
             </div>
         });
 
-        let imagesUpRight = arrayUpRight.map(image => {
-            return <div data-toggle="collapse" data-target="#collapseExample" key={image} className={(`tooth-${image}`)} onClick={() =>this.info(image)}>
-            </div> 
-        });
-
-         let imagesDownLeft = arrayDownLeft.map(image => {
+         let imagesDown = arrayDown.map(image => {
             return <div data-toggle="collapse" data-target="#collapseExample" key={image} className={(`tooth-${image}`)} onClick={() =>this.info(image)}>
                 </div>
         });
-
-         let imagesDownRight = arrayDownRight.map(image => {
-            return <div  key={image} className={(`tooth-${image}`)} onClick={() =>this.info(image)}>
-            </div>
-        });
-
-       
-        
         
       return (
         <div className="container"> 
             <div className="row">
-                <div className="col-sm-6 upTeeth">
-                    {imagesUpLeft}
-                </div>
-                <div className="col-sm-6 upTeeth">
-                    {imagesUpRight}
+                <div className="col-sm-12 upTeeth">
+                    {imagesUp}
                 </div>
             </div>
             <div className="row">
-                <div className="col-sm-6 downTeeth">
-                    {imagesDownLeft}
-                </div>
-                <div className="col-sm-6 downTeeth">
-                    {imagesDownRight}
+                <div className="col-sm-12 downTeeth">
+                    {imagesDown}
                 </div>
             </div>
             <div className="collapse" id="collapseExample">
@@ -83,15 +62,15 @@ class Jaw extends React.Component {
                     <div className="card">
                         <div class="card-header">
                             Selected Tooth visit info: {this.state.tooth}
-                        <i onClick={this.addItem}    class="fas fa-plus"></i>
                         </div>
-                    <div className="card-body">
+                    <div className="card-body service-holder">
                     {this.state.toothinfoComp.map((item, index) => {
                         return <div key={index}>
                             {item}
                         </div>
                     }
                     )}
+                    <i onClick={this.addItem} className="fas fa-plus plus">  add service...</i>
                 </div>
             </div>
             </div> 
