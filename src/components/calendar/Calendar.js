bd00aba6854482188c82f45d1b7f22118fa5b429
taras21777 <piatak.taras@gmail.com>
2018-06-06 15:41:42 +0300

    debug

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -236,7 +236,7 @@ validateForm() {
   getCurrentDateFromURL = () => {
     var currentDate = new Date(new Date());
     if(this.props.match.params.date!==null && this.props.match.params.date!==undefined)
-    // currentDate = new Date(`${this.props.match.params.date}T00:00:01`);
+    currentDate = new Date(`${this.props.match.params.date}T00:00:01`);
     currentDate = new Date(currentDate)
 
     // alert(`${this.props.match.params.date}T00:00:01`);
