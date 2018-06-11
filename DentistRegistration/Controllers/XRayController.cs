5d718aa9f1e90f062e63084a27decd3d2f251e32
nickolasmalyk <nickolas.malyk@gmail.com>
2018-06-11 12:45:35 +0300

    add xraydal

---
+++ DentistRegistration/Controllers/XRayController.cs
@@ -0,0 +1,40 @@
+﻿using DentistRegistration.DataAccessLayer;
+using DentistRegistration.Interfaces;
+using DentistRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace DentistRegistration.Controllers
+{
+    public class XRayController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult InsertXRay ([FromBody]XRay XRay)
+        {
+            XRayDAL x_Ray = new XRayDAL();
+
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                bool checkInsert = x_Ray.InsertXRay(XRay);
+
+                if (checkInsert == false)
+                    return BadRequest("Something went wrong");
+
+                return Ok(new
+                {
+                    massage = "asda"
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Something went wrong");
+            }
+        }
+    }
+}
---
+++ DentistRegistration/DataAccessLayer/XRayDAL.cs
@@ -0,0 +1,41 @@
+﻿using DentistRegistration.Interfaces;
+using DentistRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DentistRegistration.DataAccessLayer
+{
+    public class XRayDAL
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        public bool InsertXRay(XRay xrayImage)
+        {
+            bool isInserted = false;
+
+            using (var con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("spAddXRay", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@USER_ID", xrayImage.User_Id);
+                cmd.Parameters.AddWithValue("@LINK", xrayImage.Link);
+                cmd.Parameters.AddWithValue("@XRAY_DATE", xrayImage.Date.Date);
+
+                cmd.ExecuteNonQuery();
+
+                isInserted = true;
+
+
+            }
+
+        return isInserted;
+        }
+    }
+}
