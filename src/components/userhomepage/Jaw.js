776c659a937b0dbc48248dd59af16c1ecfae3e9a
Andriy Lavrushko <Ondzeylav@gmail.com>
2018-06-14 15:41:39 +0300

    fix teeth info clearing after click on another tooth

--- src/components/userhomepage/Jaw.js
+++ src/components/userhomepage/Jaw.js
@@ -10,82 +10,115 @@ class Jaw extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          services: [],
-          toothId : NaN,
-          toothinfoComp: [],
-          nextId:0,
-          activeTeeth: [],
+            services: [],
+            toothId: NaN,
+            toothinfoComp: [],
+            nextId: 0,
+            activeTeeth: [],
+            ifserviceadded: false,
+            countAddedServices: 0,
         }
         this.addItem = this.addItem.bind(this)
         this.addServiceCallback = this.addServiceCallback.bind(this)
         this.gotoBottom = this.gotoBottom.bind(this)
+        this.ToothVisitInfocontainer = this.ToothVisitInfocontainer.bind(this)
     }
-    
+
     componentDidUpdate() {
         this.gotoBottom();
     }
 
-    addServiceCallback(service, itemId)
-    {
+    addServiceCallback(service, itemId) {
         this.setState(
             this.state
         )
-        this.state.services[itemId]=service;
-        
+        this.state.services[itemId] = service;
+
     }
 
 
     info = (toothId) => {
-       this.setState({toothId : toothId})
-      return toothId;  
+        if (this.state.ifserviceadded) {
+            this.state.activeTeeth.push(this.state.toothId);
+            this.setState({ ifserviceadded: false });
+
+            for (let i = 0; i < this.state.countAddedServices; i++) {
+                this.state.toothinfoComp.pop();
+            }
+
+            let obj = this;
+            for (let i = this.state.services.length - this.state.countAddedServices; i < this.state.services.length; i++) {
+                this.state.toothinfoComp.push(<ToothVisitInfo toothId={this.state.services[i].ToothNum}
+                    desc={this.state.services[i].Description}
+                    service={this.state.services[i].ServiceId}
+                    price={this.state.services[i].Price}
+                    toothServices={this.state.services}
+                    callback={obj.addServiceCallback}
+                    itemId={this.state.services[i].Id} />)
+            }
+        }
+        this.setState({ toothId: toothId });
+        return toothId;
     }
-   
+
     addItem = () => {
- 
         let obj = this;
         this.state.toothinfoComp.push(<ToothVisitInfo toothId={this.state.toothId}
+            desc={''}
+            service={0}
+            price={0}
             toothServices={this.state.services}
-            callback={obj.addServiceCallback} itemId = {this.state.nextId}  />)
+            callback={obj.addServiceCallback} itemId={this.state.nextId} />)
         this.state.nextId++;
         this.setState(
             this.state
         )
-        this.state.activeTeeth.push(this.state.toothId);
-        //alert(JSON.stringify(this.state.services));
+        this.setState({ ifserviceadded: true });
+        this.state.countAddedServices++;
+    }
 
+    sayhello = () => {
+        alert(JSON.stringify(this.state.services));
+        this.setState({ toothId: NaN })
     }
 
     gotoBottom = () => {
         var element = document.getElementById("service-holder");
-        if(element.scrollHeight!==null){
-            element.scrollTop = element.scrollHeight};
+        if (element.scrollHeight !== null) {
+            element.scrollTop = element.scrollHeight
+        };
     }
 
-    servicescontainer(){
-        if(!isNaN(this.state.toothId)){
-            return(
+    ToothVisitInfocontainer() {
+        let serviceArray = [];
+        this.state.toothinfoComp.map((item, index) => {
+            if (item.props.toothId === this.state.toothId) {
+                serviceArray.push(<div key={index}>
+                    {item}
+                </div>)
+            }
+        })
+        return serviceArray;
+    }
+
+    servicescontainer() {
+        if (!isNaN(this.state.toothId)) {
+            return (
                 <div>
-                <div className="container">
-                    <div className="card">
-                        <div className="card-header">
-                            Tooth name: {this.state.toothId} 
-                            <span className="my-span">
-                            <i onClick={this.addItem} className="fas fa-plus plus">  add service...</i>
-                            </span>
-                        </div>
-                        <div className="card-body" id="service-holder">
-                            {this.state.toothinfoComp.map((item, index) => {
-                                if (item.props.toothId === this.state.toothId)
-                                    return <div key={index}>
-                                        {item}
-                                    </div>
-                            }
-                            )}
-                           
+                    <div className="container">
+                        <div className="card">
+                            <div className="card-header">
+                                Tooth name: {this.state.toothId}
+                                <span className="my-span">
+                                    <i onClick={this.addItem} className="fas fa-plus plus">  add service...</i>
+                                </span>
+                            </div>
+                            <div className="card-body" id="service-holder">
+                                {this.ToothVisitInfocontainer()}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             );
         }
     }
@@ -104,54 +137,51 @@ class Jaw extends React.Component {
             }
         }
         );
-        if(active){
-            return(
+        if (active) {
+            return (
                 `tooth-${id} someTeeth`
             );
         }
-        return(
+        return (
             `tooth-${id}`
         );
     }
 
-    render() { 
-        
-        let arrayUp = [18,17,16,15,14,13,12,11,21,22,23,24,25,26,27,28]
-        let arrayDown = [48,47,46,45,44,43,42,41,31,32,33,34,35,36,37,38]
-        
+    render() {
+
+        let arrayUp = [18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28]
+        let arrayDown = [48, 47, 46, 45, 44, 43, 42, 41, 31, 32, 33, 34, 35, 36, 37, 38]
+
         let imagesUp = arrayUp.map(image => {
-            return <div key={image} className={this.teethClassNames(image)} onClick={() =>this.info(image)}>
+            return <div key={image} className={this.teethClassNames(image)} onClick={() => this.info(image)}>
             </div>
         });
 
-         let imagesDown = arrayDown.map(image => {
-            return <div key={image} className={this.teethClassNames(image)} onClick={() =>this.info(image)}>
-                </div>
+        let imagesDown = arrayDown.map(image => {
+            return <div key={image} className={this.teethClassNames(image)} onClick={() => this.info(image)}>
+            </div>
         });
-        
-      return (
+
+        return (
             <div className="container">
                 <div className="row">
-                    <SummaryInfo services = {this.state.services} />
+                    <SummaryInfo services={this.state.services} />
                 </div>
-              <div className="row">
-                  <div className="upTeeth">
-                      {imagesUp}
-                  </div>
-              </div>
-              <div className="row">
-                  <div className="downTeeth">
-                      {imagesDown}
-                  </div>
-              </div>
-              {this.servicescontainer()}
-          </div>
-      )
+                <div className="row">
+                    <div className="upTeeth">
+                        {imagesUp}
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="downTeeth">
+                        {imagesDown}
+                    </div>
+                </div>
+                <button className="d-none" onClick={this.sayhello}>Hello Andriy</button>
+                {this.servicescontainer()}
+            </div>
+        )
     }
-  }
+}
 
-  
-  
-  
-  
-  export default Jaw;
+export default Jaw;
--- src/components/visitpage/ToothVisitInfo.js
+++ src/components/visitpage/ToothVisitInfo.js
@@ -10,74 +10,88 @@ class ToothVisitInfo extends React.Component {
         super(props);
         this.state = {
             'services': [],
-            service: 0,
-            price: 0,
+            service: this.props.service,
+            price: this.props.price,
             chars_left: 1024,
-            toothId: 0,
-            description: '',
+            toothId: this.props.toothId,
+            description: this.props.desc,
             toothServices: []
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSelectChange = this.handleSelectChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.optionselected = this.optionselected.bind(this);
     }
-
-    
 
     componentDidMount() {
         this.getServices();
     }
+    fireCallback() {
 
-    fireCallback()
-    {
         this.props.callback(
             {
-                Id:this.props.itemId,
+                Id: this.props.itemId,
                 XrayId: 0,
                 ServiceId: this.state.service,
                 Price: this.state.price,
                 ToothNum: this.props.toothId,
                 Description: this.state.description,
-                VisiId: 0 
+                VisiId: 0
             }, this.props.itemId
         );
     }
-    
 
-    renderInfo(){
-        return(
+    optionselected() {
+        let name = '';
+        if (this.state.service == 0) {
+            return <option disabled selected value> select a service </option>
+        }
+        else {
+            this.state.services.map((item, index) => {
+                if (item.Id == this.props.service) {
+                    name = item.Name
+                }
+                return;
+            }
+            )
+            return <option disabled selected value>{name}</option>
+        }
+    }
+
+    renderInfo() {
+        return (
             <div className="div_toothinfo">
                 <div className="row mb-1">
-                        <div className="col-md-4">
-                            <select className="form-control"  onChange={this.handleSelectChange}>
-                                <option disabled selected value> select a service </option>
-                                {this.state.services.map((item, index) => {
-                                    return <option value={item.Id} key={index}>{item.Name}</option>
-                                }
-                                )}
-                            </select>
-                        </div>
-                        <div className="col-md-2">
-                            <input className="form-control" id="focusedInput" type="text" 
+                    <div className="col-md-4">
+                        <select className="form-control" onChange={this.handleSelectChange}>
+                            {this.state.services.map((item, index) => {
+                                return <option value={item.Id} key={index}>{item.Name}</option>
+                            }
+                            )}
+                            {this.optionselected()}
+                        </select>
+                    </div>
+                    <div className="col-md-2">
+                        <input className="form-control" id="focusedInput" type="text"
                             value={this.state.price} onChange={this.handleChange} />
-                        </div>
                     </div>
-                    <div className="row mb-4">
-                        <div className="col-md-12">
-                            <span class="textarea__count">Description {this.props.toothId}: 
-                            <span className="my-span">{1024-this.state.chars_left}/1024</span></span>
-                            <textarea maxLength="1024" required onChange={this.handleWordCount} 
-                            type="text" className="form-control" name="desc" />
-                        </div>
+                </div>
+                <div className="row mb-4">
+                    <div className="col-md-12">
+                        <span class="textarea__count">Description {this.props.toothId}:
+                            <span className="my-span">{1024 - this.state.chars_left}/1024</span></span>
+                        <textarea maxLength="1024" required onChange={this.handleWordCount}
+                            type="text" className="form-control" name="desc" defaultValue={this.state.description} />
                     </div>
+                </div>
             </div>
         );
     }
 
-    componentWillUpdate(){
+    componentWillUpdate() {
         this.fireCallback()
     }
-    
+
     getServices() {
         fetchFrom('Service', 'get', null)
             .then(results => {
@@ -98,19 +112,19 @@ class ToothVisitInfo extends React.Component {
     }
 
     handleSelectChange(event) {
-        this.setState({service: event.target.value});
+        this.setState({ service: event.target.value });
         this.getCurrentPriceByService(event.target.value);
     }
-    
+
     handleSubmit(event) {
         alert('Hello, Andriy!');
         event.preventDefault();
-      }
+    }
 
     handleWordCount = event => {
         const charCount = event.target.value.length;
         const charLeft = 1024 - charCount;
-        this.setState({ chars_left: charLeft});
+        this.setState({ chars_left: charLeft });
         this.state.description = event.target.value;
     }
 
