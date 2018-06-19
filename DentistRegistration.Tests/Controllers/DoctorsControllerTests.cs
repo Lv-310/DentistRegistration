115958083877f46a2b280ff5a582e0c710b9201b
Unnamed999999999 <Unnamed999999999@Gmail.com>
2018-06-19 17:18:40 +0300

    added unit tests

---
+++ DentistRegistration.Tests/Controllers/DoctorsControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Moq;
+using DentistRegistration.Interfaces;
+using DentistRegistration.Models;
+
+namespace DentistRegistration.Controllers.Tests
+{
+    [TestFixture()]
+    public class DoctorsControllerTests
+    {
+        [Test()]
+        public void GetDoctorTest()
+        {
+            Mock<IRepositoryCRU<Doctor>> myMock = new Mock<IRepositoryCRU<Doctor>>();
+            myMock.Expect(m => m.GetById(It.IsAny<int>())).Returns((int i) => new Doctor() {Id= i, FirstName="test", LastName = "test"});
+
+            var doc = myMock.Object.GetById(8);
+            Assert.AreEqual(doc.Id, 8);
+
+            myMock.VerifyAll();
+        }
+    }
+}
---
+++ DentistRegistration.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DentistRegistration.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DentistRegistration.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13418af4-4601-4d43-8db1-11e514242e79")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
