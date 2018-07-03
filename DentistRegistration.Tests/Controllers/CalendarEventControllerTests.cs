0d003be75c7939f9b6741f7f90d1d74f507b8a01
nickolasmalyk <nickolas.malyk@gmail.com>
2018-07-03 14:05:10 +0300

    asd

---
+++ DentistRegistration.Tests/Controllers/CalendarEventControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Moq;
+using DentistRegistration.Interfaces;
+using DentistRegistration.Models;
+using DentistRegistration.DataAccessLayer;
+
+namespace DentistRegistration.Controllers.Tests
+{
+    [TestFixture()]
+    public class CalendarEventControllerTests
+    {
+        [Test()]
+        public void GetEventsByDoctorIdTest()
+        {
+            Mock<IRepositoryCRUcollection<CalendarEvent>> myMock = new Mock<IRepositoryCRUcollection<CalendarEvent>>();
+            myMock.Expect(m => m.GetById(It.IsAny<int>())).Returns((int i) => new CalendarEvent() { Id = i });
+        }
+    }
+}
