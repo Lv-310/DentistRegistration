1e4132d4a07257ce3b3c8d58d2c7e9e9645bdb22
nickolasmalyk <nickolas.malyk@gmail.com>
2018-07-03 16:26:32 +0300

    fix

--- src/helpers/azureurlandsas.js
+++ src/helpers/azureurlandsas.js
@@ -1,5 +1,5 @@
 const azureSasAndUrl = {
-Sas:'se=2018-06-30&sp=rwdlac&sv=2017-07-29&ss=b&srt=sco&sig=ClSfTtPzdu5u0%2BivPlPdoDy1dY1QUQvAeylYyZ2nC4Y%3D',
+Sas:'?sv=2017-11-09&ss=b&srt=sco&sp=rwdlac&se=2018-07-31T19:57:42Z&st=2018-07-03T11:57:42Z&spr=https,http&sig=HxcP7vrjq6DkQ6aWWwzYneT09ZRSydab5IbGWfXcbhQ%3D',
 ShortUrl:'https://lv301storage.blob.core.windows.net',
 BigUrl:'https://lv301storage.blob.core.windows.net/images/',
 avatar:'https://lv301storage.blob.core.windows.net'
